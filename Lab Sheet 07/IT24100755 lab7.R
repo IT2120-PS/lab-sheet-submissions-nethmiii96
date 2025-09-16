c915cb24c66b19dc9d0ae81ec98a30ff3d760181
Nethmi Thakshila <it24100755@my.sliit.lk>
2025-09-16 12:08:26 +0530

    lab7

---
+++ Lab Sheet 07/IT24100755 lab7.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\ASUS\\Desktop\\IT24102180")
+
+#Question1
+punif(25,min=0,max=40)-punif(10,min=0,max=40)
+
+#Question2
+pexp(2,rate=1/3,lower.tail = TRUE)
+
+#Question3-i
+1-pnorm(130,mean=100,sd=15,lower.tail = TRUE)
+
+#Question3-ii
+qnorm(0.95,mean=100,sd=15)
