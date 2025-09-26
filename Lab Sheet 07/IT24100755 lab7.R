c29c389b64b152ba7b5c38bc1fa078e4003267fe
Nethmi Thakshila <it24100755@my.sliit.lk>
2025-09-26 15:56:23 +0530

    Update ps labIT24100755

--- Lab Sheet 07/IT24100755 lab7.R
+++ Lab Sheet 07/IT24100755 lab7.R
@@ -1,4 +1,4 @@
-setwd("C:\\Users\\ASUS\\Desktop\\IT24102180")
+setwd("C:\\Users\\ASUS\\Desktop\\IT24100755")
 
 #Question1
 punif(25,min=0,max=40)-punif(10,min=0,max=40)
@@ -11,3 +11,4 @@ pexp(2,rate=1/3,lower.tail = TRUE)
 
 #Question3-ii
 qnorm(0.95,mean=100,sd=15)
+
